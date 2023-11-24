d4904cb8a6a90adf0e2796d8d7a0b3c5976fe4c0
Krishnay mudgal <kmud2208@gmail.com>
2023-11-25 00:18:25 +0530

    SMTP Port change

--- src/utils/email.js
+++ src/utils/email.js
@@ -6,7 +6,7 @@ const sendEmail = async (email, subject, text) => {
         const transporter = nodemailer.createTransport({
             host: 'sandbox.smtp.mailtrap.io',
             // service: 'gmail',
-            port: 25,
+            port: 2525,
             // secure: true,
             auth: {
                 user: 'adf2c78d349723',
@@ -24,7 +24,7 @@ const sendEmail = async (email, subject, text) => {
 
         console.log("email sent sucessfully");
     } catch (error) {
-        console.log(error, "email not sent");
+        console.log("email not sent",error.message);
     }
 };
 
