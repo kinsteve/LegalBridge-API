3fbcc8dc41f9edf07de93bcaf7150b0e98645cf2
Krishnay mudgal <kmud2208@gmail.com>
2024-05-29 21:44:38 +0530

    error fixed in findBookedSlots

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -120,7 +120,7 @@ const updateUserDetails = asyncHandler(async (req, res, next) => {
       if (!user) 
       {
         const error= new Error('User not found');
-        error.statusCode(404);
+        error.statusCode= 404;
         throw(error);
       }
       res.status(200).json(user.bookedSlots.lspId);
